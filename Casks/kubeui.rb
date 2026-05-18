16902de2c23292c8f2124f3689280923c62c08df
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-05-18 11:56:04 -0700

    wip

--- Casks/kubeui.rb
+++ Casks/kubeui.rb
@@ -4,6 +4,7 @@ cask "kubeui" do
   version "1.0.0"
   sha256 arm: "28eb125232acc9308db289afa72586ca69358fbdd637e18132351f872967210c",
          intel: "78e1b0fa0b7eca2962fef41d598398ea69658412a551325e2d767879eb872ac6"
+
   url "https://github.com/IvanJosipovic/KubeUI/releases/download/v#{version}/KubeUI-osx-#{arch}-Portable.zip",
       verified: "github.com/IvanJosipovic/KubeUI/"
   name "KubeUI"
@@ -19,7 +20,5 @@ cask "kubeui" do
 
   app "KubeUI.app"
 
-  zap trash: [
-    "~/.kubeui",
-  ]
+  zap trash: "~/.kubeui"
 end
