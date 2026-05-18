2e8d6160c84fe9fda8969d45a817f113fcb751c2
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-05-18 12:00:16 -0700

    wip

--- Casks/kubeui.rb
+++ Casks/kubeui.rb
@@ -2,7 +2,7 @@ cask "kubeui" do
   arch arm: "arm64", intel: "x64"
 
   version "1.0.0"
-  sha256 arm: "28eb125232acc9308db289afa72586ca69358fbdd637e18132351f872967210c",
+  sha256 arm:   "28eb125232acc9308db289afa72586ca69358fbdd637e18132351f872967210c",
          intel: "78e1b0fa0b7eca2962fef41d598398ea69658412a551325e2d767879eb872ac6"
 
   url "https://github.com/IvanJosipovic/KubeUI/releases/download/v#{version}/KubeUI-osx-#{arch}-Portable.zip",
@@ -16,7 +16,7 @@ cask "kubeui" do
     strategy :github_latest
   end
 
-  depends_on macos: ">= :sequoia"
+  depends_on macos: :sequoia
 
   app "KubeUI.app"
 
