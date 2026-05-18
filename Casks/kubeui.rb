415a44dfd1e6c8d3c760b7a48c3e3a0f9dafdefe
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-05-18 11:34:17 -0700

    chore: Update kubeui.rb

--- Casks/kubeui.rb
+++ Casks/kubeui.rb
@@ -6,7 +6,7 @@ cask "kubeui" do
   sha256 arm: "e48ce263aa1ea19a2ecdd880b6ba543b77d1bec88503148be94deaaba72260a1",
          intel: "adff80795e7f1bee72bf3545e2c6a322e5ee5d83fa4b120978ac331d7a6db1d4"
 
-  url "https://github.com/IvanJosipovic/KubeUI/releases/download/v#{version}/KubeUI-osx-#{arch}.zip",
+  url "https://github.com/IvanJosipovic/KubeUI/releases/download/v#{version}/KubeUI-osx-#{arch}-Portable.zip",
       verified: "github.com/IvanJosipovic/KubeUI/"
 
   name "KubeUI"
